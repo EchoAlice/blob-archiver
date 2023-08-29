d646fa95c65a39d8e71846c191de5b6ea3e4d319
EchoAlice <echoalice0@gmail.com>
2023-08-28 19:10:58 -0600

    Beginnings of blob archiver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use beacon_api_client::Client;
+
+fn process_sidecars(sidecars: Sidecar) {
+    // Write sidecars to database
+    // or...
+
+    for car in sidecars {
+        println!("{:?}", car.commitment);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cllient = Client::new("http://localhost::502");
+
+    // how do i get the id?
+    let id = [0; 32];
+    // how do i get the indices?
+    let indices = [0; 32];
+    let sidecars = client.get_blob_sidecars(id, indices).await;
+
+    process_sidecars(sidecars);
 }
