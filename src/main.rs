526b1f49981c0b937c3e61ebea8576c2caba6485
EchoAlice <echoalice0@gmail.com>
2023-08-29 09:27:17 -0600

    Initial commit (for real)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use beacon_api_client::Client;
+use beacon_api_client::{mainnet::Client, BlockId};
+use ethereum_consensus::deneb::BlobSidecar;
+use url::Url;
 
-fn process_sidecars(sidecars: Sidecar) {
-    // Write sidecars to database
-    // or...
+// How many bytes per blob?
+const BYTES_PER_BLOB: usize = 512;
 
+// TODO: Print blobs to terminal
+fn process_sidecars(sidecars: Vec<BlobSidecar<BYTES_PER_BLOB>>) {
     for car in sidecars {
-        println!("{:?}", car.commitment);
+        println!("{:?}", car.kzg_commitment);
     }
+
+    // TODO: Verify blob against commitment
+    // TODO (future): Write sidecars to database
 }
 
-fn main() {
-    let cllient = Client::new("http://localhost::502");
+#[tokio::main]
+async fn main() {
+    // Should I be using mainnet client or minimal client?
+    let url = Url::parse("http://localhost::502").unwrap();
+    let client = Client::new(url);
 
-    // how do i get the id?
-    let id = [0; 32];
-    // how do i get the indices?
+    // How do i get the block_id and indices?
+    let id = BlockId::Slot(100);
     let indices = [0; 32];
-    let sidecars = client.get_blob_sidecars(id, indices).await;
+
+    let sidecars = client.get_blob_sidecars(id, &indices).await.unwrap();
 
     process_sidecars(sidecars);
 }
