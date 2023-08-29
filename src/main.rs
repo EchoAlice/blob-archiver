d137580ac0cb03abe813f179eb57e0b3b789527a
EchoAlice <echoalice0@gmail.com>
2023-08-29 17:46:13 -0600

    Move URL logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use crate::secret::url::make_url;
 use beacon_api_client::{mainnet::Client, BlockId};
 use ethereum_consensus::deneb::BlobSidecar;
-use url::Url;
-
+mod secret;
 // How many bytes per blob? 32 x 4096
 const BYTES_PER_BLOB: usize = 131072;
 
@@ -16,23 +16,11 @@ fn process_sidecars(sidecars: Vec<BlobSidecar<BYTES_PER_BLOB>>) {
 
 #[tokio::main]
 async fn main() {
-    let username = "eth";
-    let password = "prAP7jyLJoHzbCa";
-    let devnet_name = "dencun-devnet-8";
-    let rpc_or_bn = "bn"; // Use "rpc" for the EL
-
-    let url_str = format!(
-        "https://{}:{}@{}.{}-{}-1.srv.{}.ethpandaops.io",
-        username, password, rpc_or_bn, "teku", "geth", devnet_name
-    );
-
-    let url = Url::parse(&url_str).unwrap();
+    let url = make_url();
     let client = Client::new(url);
 
-    // How do i get the block_id and indices?
     let id = BlockId::Head;
     let indices = [];
-
     let sidecars = client.get_blob_sidecars(id, &indices).await.unwrap();
     process_sidecars(sidecars);
 }
