5548c10ecd2c908d53f816a3ebdca16f66b133eb
EchoAlice <echoalice0@gmail.com>
2023-08-30 15:07:11 -0600

    Create simplified clock to print blobs from each slot

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use crate::secret::url::make_url;
+use crate::url::make_url;
 use beacon_api_client::{mainnet::Client, BlockId};
-use ethereum_consensus::deneb::BlobSidecar;
-use tokio::time::Duration;
+use ethereum_consensus::{clock::from_system_time, deneb::BlobSidecar};
+use tokio_stream::StreamExt;
 
-mod secret;
+mod url;
 
-// How many bytes per blob? 32 x 4096
-const BYTES_PER_BLOB: usize = 131072;
+const BYTES_PER_BLOB: usize = 131072; // 32 x 4096
 
-// TODO: Print blobs to terminal
+// Why are blobs only printing occasionally?
 fn process_sidecars(sidecars: Vec<BlobSidecar<BYTES_PER_BLOB>>) {
     for car in sidecars {
         println!("{:?}", car.kzg_commitment);
@@ -23,10 +22,22 @@ async fn main() {
     let url = make_url();
     let client = Client::new(url);
 
-    let id = BlockId::Head;
-    let indices = [];
+    let genesis_details = client.get_genesis_details().await.unwrap();
+    let genesis_time = genesis_details.genesis_time;
+    let seconds_per_slot = 12;
+    let slots_per_epoch = 32;
 
-    tokio::time::sleep(Duration::from_secs(1)).await;
-    let sidecars = client.get_blob_sidecars(id, &indices).await.unwrap();
-    process_sidecars(sidecars);
+    // Use Ethereum clock to query for blobs
+    let clock = from_system_time(genesis_time, seconds_per_slot, slots_per_epoch);
+    let slot_stream = clock.stream_slots();
+    tokio::pin!(slot_stream);
+
+    while let Some(slot) = slot_stream.next().await {
+        println!("Slot: {:?}", slot);
+        let id = BlockId::Head;
+        let indices = [];
+        let sidecars = client.get_blob_sidecars(id, &indices).await.unwrap();
+        process_sidecars(sidecars);
+        println!("\n");
+    }
 }
