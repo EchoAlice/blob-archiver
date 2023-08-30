05615df6fef0033054e2b33a642da28b427e80aa
EchoAlice <echoalice0@gmail.com>
2023-08-29 18:19:36 -0600

    Print blobs from Devnet 8

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use crate::secret::url::make_url;
 use beacon_api_client::{mainnet::Client, BlockId};
 use ethereum_consensus::deneb::BlobSidecar;
-<<<<<<< HEAD
+use tokio::time::Duration;
+
 mod secret;
+
 // How many bytes per blob? 32 x 4096
 const BYTES_PER_BLOB: usize = 131072;
-=======
-use url::Url;
-
-// How many bytes per blob?
-const BYTES_PER_BLOB: usize = 512;
->>>>>>> parent of 9e7212b (Get blobs from Devnet 8 and print to terminal)
 
 // TODO: Print blobs to terminal
 fn process_sidecars(sidecars: Vec<BlobSidecar<BYTES_PER_BLOB>>) {
@@ -24,23 +20,13 @@ fn process_sidecars(sidecars: Vec<BlobSidecar<BYTES_PER_BLOB>>) {
 
 #[tokio::main]
 async fn main() {
-<<<<<<< HEAD
     let url = make_url();
     let client = Client::new(url);
 
     let id = BlockId::Head;
     let indices = [];
-=======
-    // Should I be using mainnet client or minimal client?
-    let url = Url::parse("http://localhost::502").unwrap();
-    let client = Client::new(url);
 
-    // How do i get the block_id and indices?
-    let id = BlockId::Slot(100);
-    let indices = [0; 32];
-
->>>>>>> parent of 9e7212b (Get blobs from Devnet 8 and print to terminal)
+    tokio::time::sleep(Duration::from_secs(1)).await;
     let sidecars = client.get_blob_sidecars(id, &indices).await.unwrap();
-
     process_sidecars(sidecars);
 }
